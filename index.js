64193bb5d7d47f13b40a1439e679e93e5c5c739f
parvezaman <emailtoamanullah@gmail.com>
2022-02-28 20:11:11 +0600

    database connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Middleware
