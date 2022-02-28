c9c04cbda3a7e012193a5f6bb9a34dbf7ec31003
parvezaman <emailtoamanullah@gmail.com>
2022-02-28 19:40:38 +0600

    authentication error

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Mongodb Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvaku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("insertDB");
+        const haiku = database.collection("haiku");
+        // create a document to insert
+        const doc = {
+            title: "Record of a Shriveled Datum",
+            content: "No bytes, no problem. Just insert a document, in MongoDB",
+        }
+        const result = await haiku.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('wellcome to the HAWK e-scooter server');
