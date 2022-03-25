b8880940f6c21fa43b7a1d60127478d6b4b0008b
parvezaman <emailtoamanullah@gmail.com>
2022-03-25 14:50:10 +0600

    cors taken upward

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require("cors");
+app.use(cors());
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -16,7 +17,6 @@ app.use(
 );
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Mongodb Connection
